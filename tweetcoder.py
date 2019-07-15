f2db24c5dd5b185f0d078db8f61b3bcf5bd047f5
crishernandezmaps <cris@crishernandez.co>
2019-07-15 10:56:52 -0400

    ordering files and folders

--- tweetcoder.py
+++ tweetcoder.py
@@ -4,78 +4,53 @@ import logging
 from config import create_api
 import time
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 
-def get_average_rent():
-    df = pd.read_csv('comunas.csv')
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
-    urls = []
-    for comuna in df.comuna.values:
-        for page in range(1, 251):
-            url = f'https://www.portalinmobiliario.com/arriendo/departamento/{comuna}-metropolitana?pg={page}'
-            urls.append(url)
-        
-    for single_url_per_comuna in urls:
-        request_content = requests.get(single_url_per_comuna)
-        getting_content = request_content.content
-        soup = BeautifulSoup(getting_content,'html.parser')
-        property_value = soup.findAll(attrs={"name" : "data-price"})
-        print(property_value)
-        time.sleep(10)
-        
+def check_mentions(api, keywords, since_id):
+    logger.info("Retrieving mentions")
+    new_since_id = since_id
+    for tweet in tweepy.Cursor(api.mentions_timeline,since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+        if tweet.in_reply_to_status_id is not None:
+            continue
+        if any(keyword in tweet.text.lower() for keyword in keywords):
+            returned_id = tweet.user.id
+            returned_name = tweet.user.name
+            returned_text = tweet.text
+            returned_location = None
+            returned_place = None
+            if(tweet.user.location):
+                returned_location = tweet.user.location
+            if(tweet.place):
+                returned_place = tweet.place
 
+            final_result = {
+                'id': returned_id,
+                'name': returned_name,
+                'text': returned_text.strip(),
+                'location': returned_location,
+                'place': returned_place
+            }
 
-    return urls
+            logger.info(f"User: {final_result}")
+            print(final_result)
 
+            api.update_status(
+                status="Please reach us via DM",
+                in_reply_to_status_id=tweet.id,
+            )
 
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger()
-
-# def check_mentions(api, keywords, since_id):
-#     logger.info("Retrieving mentions")
-#     new_since_id = since_id
-#     for tweet in tweepy.Cursor(api.mentions_timeline,since_id=since_id).items():
-#         new_since_id = max(tweet.id, new_since_id)
-#         if tweet.in_reply_to_status_id is not None:
-#             continue
-#         if any(keyword in tweet.text.lower() for keyword in keywords):
-#             returned_id = tweet.user.id
-#             returned_name = tweet.user.name
-#             returned_text = tweet.text
-#             returned_location = None
-#             returned_place = None
-#             if(tweet.user.location):
-#                 returned_location = tweet.user.location
-#             if(tweet.place):
-#                 returned_place = tweet.place
-
-#             final_result = {
-#                 'id': returned_id,
-#                 'name': returned_name,
-#                 'text': returned_text.strip(),
-#                 'location': returned_location,
-#                 'place': returned_place
-#             }
-
-#             logger.info(f"User: {final_result}")
-#             print(final_result)
-
-#             api.update_status(
-#                 status="Please reach us via DM",
-#                 in_reply_to_status_id=tweet.id,
-#             )
-
-#     return new_since_id
+    return new_since_id
 
 def main():
-    get_average_rent()
-    # api = create_api()
-    # since_id = 1
-    # while True:
-    #     # print(since_id)
-    #     since_id = check_mentions(api, ['wheretolive'], since_id)
-    #     time.sleep(60)
+    api = create_api()
+    since_id = 1
+    while True:
+        # print(since_id)
+        since_id = check_mentions(api, ['wheretolive'], since_id)
+        time.sleep(60)
 
 if __name__ == "__main__":
     main()
